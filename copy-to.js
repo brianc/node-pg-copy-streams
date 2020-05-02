dfb233327c652fb4ee7f314aef125d0ef17fb587
Panu Pitkämäki <pspi@users.noreply.github.com>
2020-05-02 09:06:56 +0300

    Use ES6 classes

--- copy-to.js
+++ copy-to.js
@@ -6,7 +6,6 @@ module.exports = function (txt, options) {
 
 const Transform = require('stream').Transform
 const BufferList = require('obuf')
-const util = require('util')
 const code = require('./message-formats')
 
 // decoder states
@@ -14,115 +13,115 @@ const PG_CODE = 0
 const PG_LENGTH = 1
 const PG_MESSAGE = 2
 
-const CopyStreamQuery = function (text, options) {
-  Transform.call(this, options)
-  this.text = text
-  this.rowCount = 0
-  this._state = PG_CODE
-  this._buffer = new BufferList()
-  this._unreadMessageContentLength = 0
-  this._copyDataChunks = new BufferList()
-}
+class CopyStreamQuery extends Transform {
+  constructor(text, options) {
+    super(options)
+    this.text = text
+    this.rowCount = 0
+    this._state = PG_CODE
+    this._buffer = new BufferList()
+    this._unreadMessageContentLength = 0
+    this._copyDataChunks = new BufferList()
+  }
 
-util.inherits(CopyStreamQuery, Transform)
+  submit(connection) {
+    connection.query(this.text)
+    this.connection = connection
+    this.connection.removeAllListeners('copyData')
+    connection.stream.pipe(this)
+  }
 
-CopyStreamQuery.prototype.submit = function (connection) {
-  connection.query(this.text)
-  this.connection = connection
-  this.connection.removeAllListeners('copyData')
-  connection.stream.pipe(this)
-}
+  _detach() {
+    const connectionStream = this.connection.stream
+    connectionStream.unpipe(this)
 
-CopyStreamQuery.prototype._detach = function () {
-  const connectionStream = this.connection.stream
-  connectionStream.unpipe(this)
+    // unpipe can pause the stream but also underlying onData event can potentially pause the stream because of hitting
+    // the highWaterMark and pausing the stream, so we resume the stream in the next tick after the underlying onData
+    // event has finished
+    process.nextTick(function () {
+      connectionStream.resume()
+    })
+  }
 
-  // unpipe can pause the stream but also underlying onData event can potentially pause the stream because of hitting
-  // the highWaterMark and pausing the stream, so we resume the stream in the next tick after the underlying onData
-  // event has finished
-  process.nextTick(function () {
-    connectionStream.resume()
-  })
-}
+  _cleanup() {
+    this._buffer = null
+    this._copyDataChunks = null
+  }
 
-CopyStreamQuery.prototype._cleanup = function () {
-  this._buffer = null
-  this._copyDataChunks = null
-}
+  _transform(chunk, enc, cb) {
+    let done = false
+    this._buffer.push(chunk)
 
-CopyStreamQuery.prototype._transform = function (chunk, enc, cb) {
-  let done = false
-  this._buffer.push(chunk)
-
-  while (this._buffer.size > 0) {
-    if (PG_CODE === this._state) {
-      if (!this._buffer.has(1)) break
-      this._code = this._buffer.readUInt8()
-      this._state = PG_LENGTH
-    }
-
-    if (PG_LENGTH === this._state) {
-      if (!this._buffer.has(4)) break
-      this._unreadMessageContentLength = this._buffer.readUInt32BE() - 4
-      this._state = PG_MESSAGE
-    }
-
-    if (PG_MESSAGE === this._state) {
-      if (this._unreadMessageContentLength > 0 && this._buffer.size > 0) {
-        const n = Math.min(this._buffer.size, this._unreadMessageContentLength)
-        const copyDataChunk = this._buffer.take(n)
-        this._unreadMessageContentLength -= n
-        if (this._code === code.CopyData) {
-          this._copyDataChunks.push(copyDataChunk)
-        }
+    while (this._buffer.size > 0) {
+      if (PG_CODE === this._state) {
+        if (!this._buffer.has(1)) break
+        this._code = this._buffer.readUInt8()
+        this._state = PG_LENGTH
       }
 
-      if (this._unreadMessageContentLength === 0) {
-        // a full message has been captured
-        switch (this._code) {
-          case code.CopyOutResponse:
-            break
-          case code.CopyData:
-            this.rowCount++
-            break
-          // standard interspersed messages.
-          // see https://www.postgresql.org/docs/9.6/protocol-flow.html#PROTOCOL-COPY
-          case code.ParameterStatus:
-          case code.NoticeResponse:
-          case code.NotificationResponse:
-            break
-          case code.CopyDone:
-          case code.ErrorResponse:
-          default:
-            done = true
-            break
+      if (PG_LENGTH === this._state) {
+        if (!this._buffer.has(4)) break
+        this._unreadMessageContentLength = this._buffer.readUInt32BE() - 4
+        this._state = PG_MESSAGE
+      }
+
+      if (PG_MESSAGE === this._state) {
+        if (this._unreadMessageContentLength > 0 && this._buffer.size > 0) {
+          const n = Math.min(this._buffer.size, this._unreadMessageContentLength)
+          const copyDataChunk = this._buffer.take(n)
+          this._unreadMessageContentLength -= n
+          if (this._code === code.CopyData) {
+            this._copyDataChunks.push(copyDataChunk)
+          }
         }
-        this._state = PG_CODE
+
+        if (this._unreadMessageContentLength === 0) {
+          // a full message has been captured
+          switch (this._code) {
+            case code.CopyOutResponse:
+              break
+            case code.CopyData:
+              this.rowCount++
+              break
+            // standard interspersed messages.
+            // see https://www.postgresql.org/docs/9.6/protocol-flow.html#PROTOCOL-COPY
+            case code.ParameterStatus:
+            case code.NoticeResponse:
+            case code.NotificationResponse:
+              break
+            case code.CopyDone:
+            case code.ErrorResponse:
+            default:
+              done = true
+              break
+          }
+          this._state = PG_CODE
+        }
       }
     }
+
+    // flush data if any data has been captured
+    const len = this._copyDataChunks.size
+    if (len > 0) {
+      this.push(this._copyDataChunks.take(len))
+    }
+
+    if (done) {
+      this._detach()
+      this.push(null)
+      this._cleanup()
+    }
+
+    cb()
   }
 
-  // flush data if any data has been captured
-  const len = this._copyDataChunks.size
-  if (len > 0) {
-    this.push(this._copyDataChunks.take(len))
+  handleError(e) {
+    this.emit('error', e)
   }
 
-  if (done) {
-    this._detach()
-    this.push(null)
-    this._cleanup()
-  }
+  handleCopyData(chunk) {}
 
-  cb()
+  handleCommandComplete() {}
+
+  handleReadyForQuery() {}
 }
-
-CopyStreamQuery.prototype.handleError = function (e) {
-  this.emit('error', e)
-}
-
-CopyStreamQuery.prototype.handleCopyData = function (chunk) {}
-
-CopyStreamQuery.prototype.handleCommandComplete = function () {}
-
-CopyStreamQuery.prototype.handleReadyForQuery = function () {}
--- index.js
+++ index.js
@@ -11,63 +11,62 @@ module.exports = {
 }
 
 const Transform = require('stream').Transform
-const util = require('util')
 const code = require('./message-formats')
 
-const CopyStreamQuery = function (text, options) {
-  Transform.call(this, options)
-  this.text = text
-  this._listeners = null
-  this._copyOutResponse = null
-  this.rowCount = 0
-}
-
-util.inherits(CopyStreamQuery, Transform)
-
-CopyStreamQuery.prototype.submit = function (connection) {
-  this.connection = connection
-  connection.query(this.text)
-}
-
-CopyStreamQuery.prototype._transform = function (chunk, enc, cb) {
-  const Int32Len = 4
-  const lenBuffer = Buffer.from([code.CopyData, 0, 0, 0, 0])
-  lenBuffer.writeUInt32BE(chunk.length + Int32Len, 1)
-  this.push(lenBuffer)
-  this.push(chunk)
-  cb()
-}
-
-CopyStreamQuery.prototype._flush = function (cb) {
-  const Int32Len = 4
-  const finBuffer = Buffer.from([code.CopyDone, 0, 0, 0, Int32Len])
-  this.push(finBuffer)
-  this.cb_flush = cb
-}
-
-CopyStreamQuery.prototype.handleError = function (e) {
-  this.emit('error', e)
-}
-
-CopyStreamQuery.prototype.handleCopyInResponse = function (connection) {
-  this.pipe(connection.stream, { end: false })
-}
-
-CopyStreamQuery.prototype.handleCommandComplete = function (msg) {
-  // Parse affected row count as in
-  // https://github.com/brianc/node-postgres/blob/35e5567f86774f808c2a8518dd312b8aa3586693/lib/result.js#L37
-  const match = /COPY (\d+)/.exec((msg || {}).text)
-  if (match) {
-    this.rowCount = parseInt(match[1], 10)
+class CopyStreamQuery extends Transform {
+  constructor(text, options) {
+    super(options)
+    this.text = text
+    this._listeners = null
+    this._copyOutResponse = null
+    this.rowCount = 0
   }
 
-  // we delay the _flush cb so that the 'end' event is
-  // triggered after CommandComplete
-  this.cb_flush()
+  submit(connection) {
+    this.connection = connection
+    connection.query(this.text)
+  }
 
-  // unpipe from connection
-  this.unpipe(this.connection.stream)
-  this.connection = null
+  _transform(chunk, enc, cb) {
+    const Int32Len = 4
+    const lenBuffer = Buffer.from([code.CopyData, 0, 0, 0, 0])
+    lenBuffer.writeUInt32BE(chunk.length + Int32Len, 1)
+    this.push(lenBuffer)
+    this.push(chunk)
+    cb()
+  }
+
+  _flush(cb) {
+    const Int32Len = 4
+    const finBuffer = Buffer.from([code.CopyDone, 0, 0, 0, Int32Len])
+    this.push(finBuffer)
+    this.cb_flush = cb
+  }
+
+  handleError(e) {
+    this.emit('error', e)
+  }
+
+  handleCopyInResponse(connection) {
+    this.pipe(connection.stream, { end: false })
+  }
+
+  handleCommandComplete(msg) {
+    // Parse affected row count as in
+    // https://github.com/brianc/node-postgres/blob/35e5567f86774f808c2a8518dd312b8aa3586693/lib/result.js#L37
+    const match = /COPY (\d+)/.exec((msg || {}).text)
+    if (match) {
+      this.rowCount = parseInt(match[1], 10)
+    }
+
+    // we delay the _flush cb so that the 'end' event is
+    // triggered after CommandComplete
+    this.cb_flush()
+
+    // unpipe from connection
+    this.unpipe(this.connection.stream)
+    this.connection = null
+  }
+
+  handleReadyForQuery() {}
 }
-
-CopyStreamQuery.prototype.handleReadyForQuery = function () {}

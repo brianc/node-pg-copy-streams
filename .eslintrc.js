a302c6d254374df82d9adec4282d0cd7640a0c35
Panu Pitkämäki <pspi@users.noreply.github.com>
2020-05-02 08:34:14 +0300

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: ['prettier'],
+  extends: ['plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es6: true,
+    mocha: true,
+  },
+  rules: {
+    'prefer-const': ['error'],
+    'no-var': ['error'],
+  },
+}

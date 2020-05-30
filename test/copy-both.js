786a2a78ed283f8158a6500703edaf6e29ba56c1
jeromew <jerome.wagner@m4x.org>
2020-05-30 14:53:31 +0000

    test travis integration with pg replication mode

---
+++ test/copy-both.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const assert = require('assert')
+
+const pg = require('pg')
+const async = require('async')
+
+describe('copy-both', () => {
+  describe('integration tests (postgres)', () => {
+    function getClient(cb) {
+      const client = new pg.Client({
+        replication: 'database',
+      })
+      client.connect(cb)
+      return client
+    }
+
+    it('env should accept replication mode', (done) => {
+      const client = getClient((err) => {
+        if (err) return done(err)
+        const sql = [
+          `CREATE_REPLICATION_SLOT slotplug LOGICAL test_decoding`,
+          `DROP_REPLICATION_SLOT slotplug`
+        ]
+
+      async.eachSeries(sql, client.query.bind(client), function (err) {
+        client.end()
+        done(err)
+      })
+
+     }) 
+    })
+
+  })
+})

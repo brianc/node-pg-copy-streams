0ca5b3246585f33b2c39ac3df1818f7c1d40a100
jeromew <jerome.wagner@m4x.org>
2020-05-30 14:56:21 +0000

    Fix eslint for test/copy-both.js

--- test/copy-both.js
+++ test/copy-both.js
@@ -18,18 +18,13 @@ describe('copy-both', () => {
     it('env should accept replication mode', (done) => {
       const client = getClient((err) => {
         if (err) return done(err)
-        const sql = [
-          `CREATE_REPLICATION_SLOT slotplug LOGICAL test_decoding`,
-          `DROP_REPLICATION_SLOT slotplug`
-        ]
+        const sql = [`CREATE_REPLICATION_SLOT slotplug LOGICAL test_decoding`, `DROP_REPLICATION_SLOT slotplug`]
 
-      async.eachSeries(sql, client.query.bind(client), function (err) {
-        client.end()
-        done(err)
+        async.eachSeries(sql, client.query.bind(client), function (err) {
+          client.end()
+          done(err)
+        })
       })
-
-     }) 
     })
-
   })
 })
